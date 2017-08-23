91a2f84e9f93cf89c2dbe89b6755cbf5d864c742
Wei Shuai <cpuwolf@gmail.com>
2017-08-23 16:41:17 +0800

    logic is done

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# touchdownrecorder
+# TouchDownRecorder
 X-Plane 10/11 aircraft landing touch down flight recorder
+
+TouchDownRecorder .lua scripts is based on FlyWithLua plugin for X-Plane
--- touchdownrecorder.lua
+++ touchdownrecorder.lua
@@ -8,38 +8,61 @@
 --
 -- Just copy this file into the "Scripts" directory of FlyWithLua.
 --
--- a TouchDown Graphic will be printed on your screen when airplane is down to the ground
+-- a TouchDown Graphic will be printed on your screen when airplane touch down to the ground
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 
 require "graphics"
 
 local touchdown_vs_table = {}
+local touchdown_g_table = {}
 local max_table_elements = 500
+
 show_touchdown_recorder = false
+collect_touchdown_data = true
+ground_counter = 0
 
 local lastVS = 1.0
 local lastG = 1.0
 
-local gearKoofRef = XPLMFindDataRef("sim/flightmodel/forces/faxil_gear");
+local gearFRef = XPLMFindDataRef("sim/flightmodel/forces/faxil_gear");
 local gForceRef = XPLMFindDataRef("sim/flightmodel2/misc/gforce_normal");
 local vertSpeedRef = XPLMFindDataRef("sim/flightmodel/position/vh_ind_fpm2");
 
-function draw_touchdown_graph()
-    -- do nothing when the function isn't wanted
-    if show_touchdown_recorder == false then return end
 
+function is_on_ground()
+    if 0.0 ~= XPLMGetDataf(gearFRef) then
+        return 1
+        -- LAND
+    end
+
+    return 0
+    -- AIR
+end
+
+function collect_flight_data()
     lastVS = math.floor(XPLMGetDataf(vertSpeedRef));
-    lastG = math.floor(XPLMGetDataf(gForceRef));
+    lastG = XPLMGetDataf(gForceRef);
     
     -- fill the table
     table.insert(touchdown_vs_table, lastVS)
+    table.insert(touchdown_g_table, lastG)
     
     -- limit the table size to the given maximum
     if table.maxn(touchdown_vs_table) > max_table_elements then
         table.remove(touchdown_vs_table, 1)
+        table.remove(touchdown_g_table, 1)
     end
+end
+
+function draw_touchdown_graph()
+    if collect_touchdown_data == true then
+        collect_flight_data()
+    end
+
+    -- dont draw when the function isn't wanted
+    if show_touchdown_recorder == false then return end
     
-    -- draw touch down graph
+    -- draw touch down chart
     -- draw background first
     local x = (SCREEN_WIDTH / 2) - max_table_elements
     local y = (SCREEN_HIGHT / 2) - 100
@@ -47,20 +70,29 @@ function draw_touchdown_graph()
     graphics.set_color(0, 0, 0, 0.75)
     graphics.draw_rectangle(x, y, x + (max_table_elements * 2), y + 200)
     
-    -- calculate the maximum
-    local max_vs_recorded = 0
+    -- calculate max vspeed
     for k, v in pairs(touchdown_vs_table) do
         if v > max_vs_recorded then
             max_vs_recorded = v
         end
     end
+    --local max_vs_recorded = 1200.0
+
+    -- calculate max gforce
+    for k, g in pairs(touchdown_g_table) do
+        if g > max_g_recorded then
+            max_g_recorded = g
+        end
+    end
+    --local max_g_recorded = 2.2
     
-    -- and print it on the screen
+    -- and print on the screen
     graphics.set_color(1, 1, 1, 1)
     graphics.draw_line(x + (max_table_elements * 2), y + 200, x + (max_table_elements * 2) + 10, y + 200)
     graphics.draw_line(x + (max_table_elements * 2), y, x + (max_table_elements * 2) + 10, y)
-    draw_string_Helvetica_12(x + (max_table_elements * 2) + 15, y, "  0 fpm")
-    draw_string_Helvetica_12(x + (max_table_elements * 2) + 15, y + 200, tostring(max_vs_recorded).." fpm")
+    draw_string_Helvetica_12(x + (max_table_elements * 2) + 15, y, "Max "..string.format("%.02f", max_g_recorded).." G")
+    draw_string_Helvetica_12(x + (max_table_elements * 2) + 15, y + 200, "Max "..tostring(max_vs_recorded).." fpm")
+    draw_string_Helvetica_12(x, y, "TouchDownRecorder V1.0")
     
     -- now draw the chart line
     graphics.set_color(0, 1, 0, 1)
@@ -68,11 +100,50 @@ function draw_touchdown_graph()
     local last_vs_recorded = touchdown_vs_table[1]
     for k, v in pairs(touchdown_vs_table) do
         graphics.draw_line(x, y + (last_vs_recorded / max_vs_recorded * 200), x + 2, y + (v / max_vs_recorded * 200))
+        if v == max_vs_recorded then
+            graphics.draw_line(x, y, x, y + (v / max_vs_recorded * 200))
+        end
         x = x + 2
         last_vs_recorded = v
     end
+    -- now draw the chart line
+    graphics.set_color(0, 0, 1, 1)
+    graphics.set_width(3)
+    local last_g_recorded = touchdown_g_table[1]
+    for k, g in pairs(touchdown_g_table) do
+        graphics.draw_line(x, y + (last_g_recorded / max_g_recorded * 200), x + 2, y + (g / max_g_recorded * 200))
+        if g == max_g_recorded then
+            graphics.draw_line(x, y, x, y + (g / max_g_recorded * 200))
+        end
+        x = x + 2
+        last_g_recorded = g
+    end
 end
 
+function calc_touchdown()
+    is_gnd = is_on_ground()
+    if is_gnd then
+        ground_counter = ground_counter + 1
+        -- ignore debounce
+        if ground_counter == 2 then
+            show_touchdown_recorder = true
+        end
+        -- stop data collection
+        if ground_counter == 10 then
+            collect_touchdown_data = false
+        end
+        -- hide chart
+        if ground_counter > 30 then
+            show_touchdown_recorder = false
+        end
+    else
+        ground_counter = 0
+        collect_touchdown_data = true
+    end
+end
+
+
 do_every_draw("draw_touchdown_graph()")
+do_often("calc_touchdown()")
 
 add_macro("Show TouchDown Recorder", "show_touchdown_recorder = true", "show_touchdown_recorder = false", "deactivate")
